a6305a7e96f119d77decf6f9f3f432504d3397b7
katsunobu1008 <katsunobu1008@gmail.com>
2025-09-23 17:08:03 +0900

    Spring Initializrで初期設定

---
+++ backend/src/test/java/com/example/picsy_engine/PicsyEngineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.picsy_engine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicsyEngineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
